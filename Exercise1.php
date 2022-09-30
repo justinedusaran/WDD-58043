20942b1cec8a0e2d082a148b7b2a0ac4c9a355a1
Justine Marie Dusaran <84701462+justinedusaran@users.noreply.github.com>
2022-09-30 19:01:52 +0800

    Add files via upload

---
+++ Exercise1.php
@@ -0,0 +1,20 @@
+<html>
+<head><title>Exercise 1</title></head>
+<body bgcolor = "FADCD9" >
+<?php
+	
+	$FullName = "Justine Marie R. Dusaran";
+	echo $FullName;
+	echo "<br>";
+	$CollegeProgram = "BS Computer Engineering";
+	echo $CollegeProgram;
+	echo "<br>";
+	$YearandSection = "3rd Year - 58043";
+	echo $YearandSection;
+	echo "<br>";
+	echo $string = "Today, I will learn PHP Programming";
+	
+?>
+
+</body>
+</html>
